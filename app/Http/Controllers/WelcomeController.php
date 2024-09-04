1a0464de3fc15b46fd407a560932dac65df8980a
Christian Fernando Saragih <chrissumbayak0@gmail.com>
2024-09-04 11:45:50 +0700

    JobSheet 2 _ Praktikum 3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers; use Illuminate\Http\Request;
 class WelcomeController extends Controller
 {
-public function hello() {
-return 'Hello World';
+
+    public function hello(){ return('Hello World');
+    }
+        
+    public function greeting(){ return view('blogs.hello')
+    ->with('name','Christian Fernando')
+    ->with('occupation','Astronaut');
+    }
 }
-}
+        
